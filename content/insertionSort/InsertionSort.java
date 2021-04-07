35d4416b08959752536d3476a9ee5e26da64bd9e
bearbaba <2200522850@qq.com>
2021-04-07 17:17:39 +0800

    插入排序

--- content/insertionSort/InsertionSort.java
+++ content/insertionSort/InsertionSort.java
@@ -2,7 +2,7 @@
 /*
  * @Author: beardada
  * @Date: 2021-04-07 09:30:14
- * @LastEditTime: 2021-04-07 17:11:08
+ * @LastEditTime: 2021-04-07 17:17:15
  * @LastEditors: Please set LastEditors
  * @Description: In User Settings Edit
  * @FilePath: \content\insertionSort\InsertionSort.java
@@ -13,15 +13,23 @@ public class InsertionSort {
   public static void sort(int[] list) {
     for (int i = 1; i < list.length; i++) {
       int temp = list[i];
-      for (int j = i - 1; j > 0 && list[j] < temp; j--) {
+      int j;
+      for (j = i - 1; j > 0 && list[j] > temp; j--) {
         list[j + 1] = list[j];
       }
       list[j + 1] = temp;
     }
   }
 
-  // TODO: main 函数未写完全
-  public static void main(String[] args) {
+  public static void print(int[] list) {
+    for (int i : list) {
+      System.out.println(i);
+    }
+  }
 
+  public static void main(String[] args) {
+    int[] l = { 1, 4, 2, 3, 7, 6, 5 };
+    sort(l);
+    print(l);
   }
 }
